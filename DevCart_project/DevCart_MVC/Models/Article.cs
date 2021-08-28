15d871fe43a52d93396f797d89d8aa5d6fb3f72e
milad mousavi <milad.mousavi1368@gmail.com>
2021-08-28 23:18:55 +0430

    project section from index page moved to partial view project

---
+++ DevCart_project/DevCart_MVC/Models/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCart_MVC.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DevCart_project/DevCart_MVC/Models/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCart_MVC.Models
+{
+    public class IndexModel
+    {
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
