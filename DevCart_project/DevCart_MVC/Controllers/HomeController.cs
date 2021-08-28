cb9afa6fdcc7ffa9aa8288e76183fa0c656da8c0
milad mousavi <milad.mousavi1368@gmail.com>
2021-08-28 22:29:08 +0430

    index & contact page added

--- DevCart_project/DevCart_MVC/Controllers/HomeController.cs
+++ DevCart_project/DevCart_MVC/Controllers/HomeController.cs
@@ -1,21 +1,16 @@
 ﻿using DevCart_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCart_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+     
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
@@ -23,10 +18,13 @@ namespace DevCart_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
+
+     
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
